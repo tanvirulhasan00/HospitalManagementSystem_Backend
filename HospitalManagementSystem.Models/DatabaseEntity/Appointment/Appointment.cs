8135f8c0ca0d40090af4f19f1eba4a76bba4bcb5
tanvirulhasan00 <tanvirulhasanedesh@gmail.com>
2026-04-08 09:11:16 +0600

    add appointment class

---
+++ HospitalManagementSystem.Models/DatabaseEntity/Appointment/Appointment.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace HospitalManagementSystem.Models.DatabaseEntity.Appointment;
+
+public class Appointment
+{
+    public Guid Id { get; init; }
+    [Required]
+    public string AppointmentCode { get; init; } = string.Empty;
+
+    public string PatientCode { get; set; }  = string.Empty;
+    public string DepartmentName { get; set; } = string.Empty;
+    public string DoctorName { get; set; }  = string.Empty;
+    public DateTime AppointDate { get; set; }
+    public int SerialNo { get; set; }
+    public string Problem { get; set; }  = string.Empty;
+    public bool Status { get; set; }
+}
